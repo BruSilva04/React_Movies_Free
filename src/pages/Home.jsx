8a93297ac282fda82909d39d91462020542f6b54
Bruno Silva <silvabrunin503@gmail.com>
2023-09-13 14:30:45 -0300

    primeiro commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+import MoviesCard from "../components/MoviesCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKEY = import.meta.env.VITE_API_KEY;
+
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRateMovies = async (URL) => {
+    const res = await fetch(URL)
+    const data =  await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+    const topRateURL =  `${moviesURL}top_rated?${apiKEY}`
+
+    getTopRateMovies(topRateURL)
+  }, [])
+
+  return(
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Caregando...</p> }
+      {topMovies.length > 0 && topMovies.map((movie) => <MoviesCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
+  )
+}
+
+export default Home
